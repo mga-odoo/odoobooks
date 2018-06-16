9032431c500aae8ad449e20d5eb6efcc1b0fb550
Mantavya Gajjar <mga@Mantavyas-MacBook-Air.local>
2018-06-16 09:44:20 +0530

    [TEST]: add image on menu

--- docs/conf.py
+++ docs/conf.py
@@ -148,7 +148,7 @@ pygments_style = 'sphinx'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = './images/openerp-book.png'
+html_logo = './images/rsz_openerp-book.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
