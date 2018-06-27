52cea6519ffb7ffe27c6b2bbc74ecc42eff22bfe
Mantavya Gajjar <mga@openerp.com>
2018-06-27 13:05:02 +0530

    [ADD]: add mete for the site verification

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig'
+    'sphinx.ext.ifconfig',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
