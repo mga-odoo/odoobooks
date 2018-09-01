6ff6f493a99181ab19aaa6ea0e81d81fea85cd41
Mantavya Gajjar <mantavyagajjar@Mantavyas-MacBook-Pro.local>
2018-09-02 01:38:35 +0530

    [FIX]: Odoo 12.0 documention

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Odoo Online Book'
+project = u'Odoo'
 copyright = u'Mantavya Gajjar'
 author = u'Mantavya Gajjar'
 
